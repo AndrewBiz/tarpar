9d865a3e3b34687070b5b6c16b27472ee7d50a4c
Andrew Biz <andrew.bizyaev@gmail.com>
2024-05-07 20:27:13 +0300

    added comment for color

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 pub const NO_VALUE: &str = "no_value";
 
-pub const COLOR_BLACK: &str = "#000000";
-pub const COLOR_GREEN: &str = "#009900";
-pub const COLOR_BLUE: &str = "#007FFF";
-pub const COLOR_RED: &str = "#FF0000";
+pub const COLOR_BLACK: &str = "#000000"; // DEC: 0 0 0
+pub const COLOR_GREEN: &str = "#009900"; // DEC: 0 153 0
+pub const COLOR_BLUE: &str = "#007FFF"; // DEC: 0 127 255
+pub const COLOR_RED: &str = "#FF0000"; // DEC: 255 0 0
 
 pub const ACTION_USE: &str = "";
 pub const ACTION_CREATE: &str = "Создать";
