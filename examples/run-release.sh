c478f11236ea06dd9e5950d5fba6276d10ebe4d2
Andrew Biz <andrew.bizyaev@gmail.com>
2024-05-08 19:07:01 +0300

    added run-relese for bench

---
+++ examples/run-release.sh
@@ -0,0 +1,5 @@
+echo "================================="
+date -Ins
+cargo run --release -- ./examples/media/tar-fiit-example.xml >./examples/output/fiit21.txt
+date -Ins
+echo "================================="
